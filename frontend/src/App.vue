fdf2900756af01e7f2bff8dacb4741647158975b
z101340 <z101340@outlook.com>
2020-05-28 07:56:09 -0400

    install bulma, edited home page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <!-- navigation bar, only needs a home button -->
     <div id="nav">
       <router-link to="/">Home</router-link>
       <!-- test for the router links -->
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="home">
+    <!-- able to change the background -->
     this is the home of this project
+
+    <!-- different question type goes here -->
     <div class="info">
       this is the navigation page 
-      <router-link to="/multipleChoice">multipleChoice</router-link>s
+      <router-link to="/multipleChoice"> 选择题 </router-link>
+      <router-link to="/mask"> 挡脸题 </router-link>
+      <router-link to="/mosaic"> 马赛克题 </router-link>
+      <router-link to="/music"> 音乐题 </router-link>
+      <router-link to="/shortAnswer"> 简答题 </router-link>
+      <router-link to="/trueFalse"> 判断题 </router-link>
     </div>
   </div>
 </template>

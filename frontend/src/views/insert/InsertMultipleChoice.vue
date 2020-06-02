747b693cd68a9f6c37b67841bb81ef22d6df6003
z101340 <z101340@outlook.com>
2020-06-02 03:04:10 -0400

    add insert multiplechoice

--- frontend/src/views/insert/InsertMultipleChoice.vue
+++ frontend/src/views/insert/InsertMultipleChoice.vue
@@ -2,22 +2,39 @@
   <div class="multipleChoice">
     <div class="question">
       <!-- attribute.field_question -->
-      {{questions[0].field_question}}
+      <p> 题目: </p> <input v-model="question" class="input" type="text" placeholder="在这写题目">
     </div>
     <div class="answers">
-        <!-- attribute.field_a,b,c,d -->
-        <div id="optionA">{{questions[0].field_A}}</div>
-        <div id="optionB">{{questions[0].field_B}}</div>
-        <div id="optionC">{{questions[0].field_C}}</div>
-        <div id="optionD">{{questions[0].field_D}}</div>
+    <div id="optionADiv">
+        <p> 选项A: </p> <input v-model="optionA" class="input" type="text" placeholder="在这写答案">
     </div>
-    
-    <div class="correct" v-on:click=showCorrectAnswer()>
-      <a class="button"> 正确答案 </a>
+
+    <div id="optionBDiv">
+        <p> 选项B: </p> <input v-model="optionB" class="input" type="text" placeholder="在这写答案">
     </div>
-    
-    <div class="next" v-on:click=showNextQuestion()>
-      <a class="button"> 下一题 </a>
+
+    <div id="optionCDiv">
+        <p> 选项C: </p> <input v-model="optionC" class="input" type="text" placeholder="在这写答案">
+    </div>
+
+    <div id="optionDDiv">
+        <p> 选项D: </p> <input v-model="optionD" class="input" type="text" placeholder="在这写答案">
+    </div>
+
+    <div id="correctOptionDiv">
+        <p>正确选项: </p> 
+          <select v-model="selected">
+            <option>a</option>
+            <option>b</option>
+            <option>c</option>
+            <option>d</option>
+          </select>
+    </div>
+
+    </div>
+  
+    <div class="confirm">
+      <a class="button" v-on:click=confirmInsert()> 确认 </a>
     </div>
 
   </div>
@@ -25,61 +42,64 @@
 
 <script>
 export default {
-  name: 'multipleChoice',
+  name: 'insertMultipleChoice',
 
   data(){
     return {
       // questions and answers?
       // will be get from backend
-      questions: [
-        {
-          field_correct: "a",
-          field_question: "test1",
-          field_A: "A",
-          field_B: "B",
-          field_C: "C",
-          field_D: "D",
-        }
-      ]
+
    }
   },
 
   methods: {
-    // click to choose
-    showCorrectAnswer(){
-      // get correct answer
-      // get question number first will implement after construct a backend
-      var correct = this.questions[0].field_correct;
-      // show the correct answer
-      switch(correct){
-        case 'a':
-          document.getElementById('optionA').style.color = "red";
-          break;
-        case 'b':
-          document.getElementById('optionB').style.color = "red";
-          break;
-        case 'c':
-          document.getElementById('optionB').style.color = "red";
-          break;
-        case 'd':
-          document.getElementById('optionB').style.color = "red";
-          break;
-        default:
-          console.log("这get不到要出大事啊")
+      confirmInsert(){
+        if(this.question == null){
+            alert("题目没写");
+            return;
+        }
+        else if(this.optionA==null||this.optionB==null || this.optionC==null || this.optionD==null){
+            alert("有空白选项");
+            console.log("有空白的");
+            return;
+        }
+        else if(this.selected == null){
+            alert("没选正确答案");
+            return;
+        }
+        else{
+          console.log("全部ok");
+          // form a object
+          var question = {
+                      field_correct: this.selected,
+                      field_question: this.question,
+                      field_A: this.optionA,
+                      field_B: this.optionB,
+                      field_C: this.optionC,
+                      field_D: this.optionD,
+          }
+          // send to backend
+            fetch('//localhost:3000/api/insert/multipleChoice', {
+                method: 'POST',
+                credentials: 'include',
+                body: JSON.stringify(question)
+            })
+            .then(res => res.json())
+            .then((response) => {
+                if (response.success) {
+                    console.log(response);
+                } else {
+                    console.error(response);
+                }
+            }).catch((error) => {
+                console.error(error);
+            })
+        }
+
       }
-      // by default we just gonna change the color of the question
+
     },
 
-    showNextQuestion(){
-      // get next question
 
-      // replace the current question
-
-      // reset the color
-    },
-
-    // if use the drupel, we need decode first
-    // zhuanma(){}
-  }
 }
 </script>
